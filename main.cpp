bf668b20b0c73c58c9bea8c8df87c4ae419e3434
Sergei Kazantsev <skazik@nestlabs.com>
2015-09-18 10:52:28 -0700

    this is a single commit combining testing - the first commit to branch better formatting another one 4th commit to branch

--- main.cpp
+++ main.cpp
@@ -324,3 +324,39 @@ int main(int argc, char* argv[])
     // suppress
     argc++, argv++;
 }
+
+// Development NOTES
+/* -----------------------------------------------------------------------------
+ * In order to keep this easy to navigate, it is asked that you squash your commits
+ * down to a few, or one, discreet changesets before submitting a pull request. Fixing
+ * a bug will usually only need one commit, while a larger feature might contain a couple
+ * of separate improvements that is easier to track through different commits.
+ 
+ Once you have rebased your work on top of the latest state of the upstream master, you may
+ have several commits related to the issue you were working on. Once everything is done,
+ squash them into a single commit with a descriptive message, like "Issue #100: Retweet bugfix."
+
+ To squash four commits into one, do the following:
+
+    $ git rebase -i HEAD~3
+
+ In the text editor that comes up, replace the words "pick" with "squash" next to the commits
+ you want to squash into the commit before it. Save and close the editor, and git will combine
+ the "squash"'ed commits with the one before it. Git will then give you the opportunity to
+ change your commit message to something like, "Issue #100: Fixed retweet bug."
+ 
+- pick bc93be2 testing - the first commit to branch
+- squash kf832369 better formatting
+- squash ea51c94 another one
+ 
+
+ **Important: If you've already pushed commits to GitHub, and then squash them locally,
+ you will have to force the push to your branch.
+
+    $ git push origin branch-name --force
+
+
+ Helpful hint: You can always edit your last commit message, before pushing, by using:
+
+    $ git commit --amend
+*/
